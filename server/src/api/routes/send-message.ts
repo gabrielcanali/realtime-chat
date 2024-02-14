8d949a5683bbb1c5a80b07f5da209b190108f47c
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-02-13 22:22:04 -0300

    feat: create initial send message route and database operations

---
+++ server/src/api/routes/send-message.ts
@@ -0,0 +1,57 @@
+import { FastifyInstance } from "fastify";
+import z from "zod";
+import { prisma } from "../../lib/prisma";
+import { randomUUID } from "crypto";
+
+export async function sendMessage(app: FastifyInstance) {
+    app.post('/api/message/:channel', async (request, reply) => {
+        const messageBody = z.object({
+            content: z.string(),
+        })
+        const messageParam = z.object({
+            channel: z.string().uuid()
+        })
+
+        const { content } = messageBody.parse(request.body) 
+        const { channel } = messageParam.parse(request.params)
+
+        // TODO: Fix duplicate user on send message with a sessionId/User assign
+
+        let { sessionCookieId } = request.cookies
+
+        if (!sessionCookieId) {
+            sessionCookieId = randomUUID()
+
+            reply.setCookie('sessionId', sessionCookieId, {
+                path: '/',
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                signed: true,
+                httpOnly: true
+            })
+        }
+
+        let user = await prisma.user.findUnique({
+            where: {
+                sessionId: sessionCookieId
+            }
+        })
+
+        if (!user) {
+            user = await prisma.user.create({
+                data: {                        
+                    sessionId: sessionCookieId
+                }
+            })
+        }
+
+        const message = await prisma.message.create({
+            data: {
+                userId: user.id,
+                channel,
+                content,
+            }
+        })
+
+        return reply.status(201).send({ message: message })
+    })
+}
