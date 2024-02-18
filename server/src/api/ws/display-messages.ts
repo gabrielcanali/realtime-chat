9d0b547e6e49a0d38dd53ff7d94943c86163b75f
Gabriel Canali <gabrielcanali.dev@gmail.com>
2024-02-17 21:12:38 -0300

    feat: create display message websocket

--- server/src/api/ws/display-messages.ts
+++ server/src/api/ws/display-messages.ts
@@ -1,7 +1,36 @@
 import { FastifyInstance } from "fastify";
+import z from "zod";
+import { getMessageHistory } from "../../utils/message-history";
+import { messageDisplay } from "../../utils/message-pub-sub";
 
 export async function displayMessage(app: FastifyInstance) {
-    app.get('/api/:channel', {websocket: true}, (connection, request) => {
+    app.get('/api/:channel', {websocket: true}, async (connection, request) => {
+        const messageParam = z.object({
+            channel: z.string().uuid()
+        })
+
+        type Message = {
+            id?: Number,
+            userId: Number,
+            channel?: string,
+            content: string,
+            created_at: Date
+        }
+
+        const { channel } = messageParam.parse(request.params)
+
+        connection.socket.send('Getting old messages, please await')
+
+        const messageHistory = await getMessageHistory(channel)
         
+        function showMessage (message: Message) {
+            connection.socket.send(`${message.content} - UserID: ${message.userId}`)
+        }
+
+        messageHistory.forEach(message => showMessage(message))
+
+        messageDisplay.subscribe(channel, (message) => {
+            showMessage(message)
+        })
     })
 }
